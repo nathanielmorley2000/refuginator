b7d1ef11bb70f6b7fbff4b9741b0474d9381f935
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-05-01 16:50:42 -0600

    Update launchRefuginator.R
    Added roxygen2 documentation for launchRefuginator() function.

--- R/launchRefuginator.R
+++ R/launchRefuginator.R
@@ -1,3 +1,14 @@
+#' Launch the Refuginator Application in your Browser
+#'
+#' @param inbrowser Choose whether to launch the application in your default browser.
+#'
+#' @return Launches Refuginator application in your browser. All features are contained within the application.
+#' @export
+#'
+#' @examples launchRefuginator()
+#' @examples launchRefuginator(inbrowser = TRUE)
+
+
 launchRefuginator <- function(inbrowser = TRUE) {
   appDir <- system.file("shinyApp", package = "refuginator")
   if (appDir == "") {
