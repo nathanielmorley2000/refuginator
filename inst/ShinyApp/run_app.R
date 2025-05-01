3fe50b36463f7e4d1ffcc7e3845642e6afa12096
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-04-30 21:04:38 -0600

    Update run_app.R
    Added libraries and changed scipen to 9999 to avoid warning messages.

--- inst/ShinyApp/run_app.R
+++ inst/ShinyApp/run_app.R
@@ -1,6 +1,38 @@
+safely_patch_options <- function() {
+  original_options <- base::options
+
+  unlockBinding("options", baseenv())
+  assign("options", function(..., .env = parent.frame()) {
+   args <- list(...)
+    if ("scipen" %in% names(args) && args$scipen > 9999) {
+      args$scipen <- 9999
+    }
+    do.call(original_options, args, envir = .env)
+  }, envir = baseenv())
+  lockBinding("options", baseenv())
+}
+
+
+
+# Apply the patch BEFORE loading packages
+safely_patch_options()
+
 # load libraries
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+library("gganimate")
+library("gifski")
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
 library("shiny")
-library("rsconnect")
+library("shinyjs")
+library("shinycssloaders")
+library("plotly")
+library("leaflet")
+library("neotoma2")
+library("viridis")
 
 # run application
 runApp("~/GitHub/refuginator/inst/ShinyApp/")
