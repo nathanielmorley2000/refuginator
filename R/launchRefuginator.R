3e6af32c7828d440e52c785ba46f0d2b6157f8b4
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-05-21 18:06:16 -0600

    Update launchRefuginator.R
    Changed variable name to be more descriptive.

--- R/launchRefuginator.R
+++ R/launchRefuginator.R
@@ -1,19 +1,19 @@
 #' Launch the Refuginator Application in your Browser
 #'
-#' @param inbrowser Choose whether to launch the application in your default browser.
+#' @param defaultbrowser Choose whether to launch the application in your default browser.
 #'
 #' @return Launches Refuginator application in your browser. All features are contained within the application.
 #' @export
 #'
 #' @examples launchRefuginator()
-#' @examples launchRefuginator(inbrowser = TRUE)
+#' @examples launchRefuginator(defaultbrowser = TRUE)
 
 
-launchRefuginator <- function(inbrowser = TRUE) {
+launchRefuginator <- function(defaultbrowser = TRUE) {
   appDir <- system.file("shinyApp", package = "refuginator")
   if (appDir == "") {
     stop("Could not find shinyApp. Try re-installing `refuginator`.", call. = FALSE)
   }
 
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = inbrowser)
+  shiny::runApp(appDir, display.mode = "normal", launch.browser = defaultbrowser)
 }
